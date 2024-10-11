c85088b9f06ea01ed28bd23d223e5c2c25cd551a
Le Gall, Benoit <legall.benoit@gmail.com>
2024-10-11 10:16:27 +0200

    page number not serialized in Micronaut 4.6 ?

--- src/main/java/com/frogdevelopment/MyController.java
+++ src/main/java/com/frogdevelopment/MyController.java
@@ -4,6 +4,14 @@ import static io.micronaut.http.HttpResponse.ok;
 import static io.micronaut.http.HttpStatus.OK;
 import static io.micronaut.scheduling.TaskExecutors.BLOCKING;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
 import io.micronaut.http.HttpResponse;
@@ -17,11 +25,17 @@ import io.micronaut.scheduling.annotation.ExecuteOn;
 public class MyController {
 
     public static final String BASE_ENDPOINT = "/items";
+    private static final Logger log = LoggerFactory.getLogger(MyController.class);
 
     @Status(OK)
     @Get("/search")
-    public HttpResponse<Page<MyItem>> searchCampaigns(Pageable pageable) {
-        return ok(Page.empty());
+    public HttpResponse<Page<MyItem>> searchItems(Pageable pageable) {
+        final var items = IntStream.range(0, pageable.getSize())
+                .mapToObj(i -> new MyItem("field_" + i, i))
+                .collect(Collectors.toCollection(ArrayList::new));
+        final var page = Page.of(items, pageable, pageable.getSize()* 3L);
+        log.info("Searched items: {}", page);
+        return ok(page);
     }
 
 }
--- src/test/java/com/frogdevelopment/MyControllerTest.java
+++ src/test/java/com/frogdevelopment/MyControllerTest.java
@@ -1,6 +1,7 @@
 package com.frogdevelopment;
 
 import static io.micronaut.http.HttpStatus.OK;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.inject.Inject;
@@ -8,7 +9,9 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MutableHttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
@@ -21,7 +24,7 @@ class MyControllerTest {
     private HttpClient client;
 
     @Test
-    void should_getEmptyPageable_when_searchingWithNonMatchingCriteria() {
+    void should_use_default_pageable_when_not_set() {
         // Given
 
         // When
@@ -29,8 +32,30 @@ class MyControllerTest {
 
         // Then
         assertThat(response.getStatus().getCode()).isEqualTo(OK.getCode());
-        assertThat(response.getBody()).isPresent();
-        assertThat(response.getBody().get()).isEmpty();
+        assertThat(response.getBody()).hasValueSatisfying(page -> {
+            assertThat(page.getContent()).hasSize(100);
+            assertThat(page.getPageable().getSize()).isEqualTo(100);
+            assertThat(page.getPageable().getNumber()).isZero();
+            assertThat(page.getTotalSize()).isEqualTo(300);
+        });
+    }
+
+    @Test
+    void should_getPage_with_pageable_values() {
+        // Given
+        final var request = HttpRequest.GET("/search?page=3&size=33");
+
+        // When
+        var response = client.toBlocking().exchange(request, Page.class);
+
+        // Then
+        assertThat(response.getStatus().getCode()).isEqualTo(OK.getCode());
+        assertThat(response.getBody()).hasValueSatisfying(page -> {
+            assertThat(page.getContent()).hasSize(33);
+            assertThat(page.getPageable().getSize()).isEqualTo(33);
+            assertThat(page.getPageable().getNumber()).isEqualTo(3);
+            assertThat(page.getTotalSize()).isEqualTo(99);
+        });
     }
 
 }
