433d5ee41221c013517cef799fe9740701bfb0f6
Le Gall, Benoit <legall.benoit@gmail.com>
2024-12-11 16:17:06 +0100

    add open-api

--- src/main/java/com/frogdevelopment/Application.java
+++ src/main/java/com/frogdevelopment/Application.java
@@ -1,7 +1,20 @@
 package com.frogdevelopment;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hello World",
+                version = "${demo.version}",
+                description = "Application demo for bug report",
+                license = @License(name = "Apache 2.0", url = "https://github.com/FrogDevelopment"),
+                contact = @Contact(url = "https://github.com/FrogDevelopper", name = "FrogDevelopper", email = "frog.development.it@gmail.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/frogdevelopment/MyItem.java
+++ src/main/java/com/frogdevelopment/MyItem.java
@@ -1,7 +1,15 @@
 package com.frogdevelopment;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 import io.micronaut.serde.annotation.Serdeable;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Serdeable
-public record MyItem(String field1, Integer field2) {
+public record MyItem(
+        @Schema(description = "My 1st field", requiredMode = REQUIRED)
+        String field1,
+        @Schema(description = "An other field", requiredMode = NOT_REQUIRED)
+        Integer field2) {
 }
